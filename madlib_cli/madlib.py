e6923f5b43d6159e32826698e5689a1b16b7a161
Domaine Scully <dalexscully@gmail.com>
2022-12-24 21:48:16 -0500

    revised lab 3

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,54 +1,63 @@
 import re
 
 
-def greeting():
-    print("Thank you for coming to Madlib: A informative game where you can use random words and place into your story.")
+def greeting_msg():
+    print(""" Welcome to my madlib-cli. Please follow the prompts to enter various types of words.
+     These words will automatically be used to fill in blanks of a narrative """)
 
 
-def read_template(read_file):
-    with open(read_file) as file:
-        try:
-            return file.read().strip()
-        except FileNotFoundError:
-            return 'File not Found'
+def read_template(path):
+    # This function will render a template madlib file
+    try:
+        with open(path, 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError as fnf_error:
+        raise fnf_error
 
 
 def parse_template(string):
+    """
+    This function will parse the template that was read in the read_template
+    function.
+    """
     new_string = string
 
     parts = tuple(re.findall(r"\{(.*?)\}", new_string))
     string = re.sub(r"\{(.*?)\}", "{}", new_string)
 
     print(string, parts)
-    return (string, parts)
+    return string, parts
 
 
-def merge(str, tuple):
-    return str.format(*tuple)
+def merge(narrative, user_input):
+    """
+     This function will combine user input with the given story and return it.
+     """
+    complete_narrative = narrative.format(*user_input)
+    return complete_narrative
+
+
+def main(path):
+    template = read_template(path)
+
+    string, parts = parse_template(template)
+
+    new_parts = []
+
+    for word in parts:
+        new_word = input(f"Enter a {word}. ")
+        new_parts.append(new_word)
+
+    madlib = merge(string, new_parts)
+
+    with open('assets/new_file.txt', 'w') as file:
+        file.write(madlib)
+
+    print(f'Your madlib is: {madlib}')
 
 
 if __name__ == "__main__":
-    path = 'assets/dark_and_stormy_night_template.txt'
-    print(read_template(path))
-
-
-
-# greeting()
-#
-# template = read_template('assets/dark_and_stormy_night_template.txt')
-#
-# string, parts = parse_template(template)
-#
-# new_parts = []
-#
-# for word in parts:
-#     new_word = input(f"Enter a {word}. ")
-#     new_parts.append(new_word)
-#
-# madlib = merge(string, new_parts)
-#
-# with open('../assets/new_file.txt', 'w') as file:
-#     file.write(madlib)
-#
-#
-# print(f'Your madlib is: {madlib}')
+    greeting_msg()
+    path = 'assets/make_me_a_video_game_template.txt'
+    main(path)
