802e965900bf59868a7bde392c5f0c3bee99263a
Domaine Scully <dalexscully@gmail.com>
2022-12-28 11:39:56 -0500

    Revised Lab 3

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,63 +1,93 @@
 import re
 
 
-def greeting_msg():
-    print(""" Welcome to my madlib-cli. Please follow the prompts to enter various types of words.
-     These words will automatically be used to fill in blanks of a narrative """)
+def print_welcome_message():
+    print(
+        """
+*******  Welcome to The MadLib Game!! *******  
+A series of questions will be asked. 
+
+Please answer all questions
+
+      """
+    )
 
 
-def read_template(path):
-    # This function will render a template madlib file
+def read_template(location_of_file):
+    """
+    INPUT >> path to file
+    OUTPUT >> raw string of everything in file
+    """
     try:
-        with open(path, 'r') as file:
+        with open(location_of_file, "r") as file:
             content = file.read()
             return content
     except FileNotFoundError as fnf_error:
-        raise fnf_error
+        print("An error was encountered when grabbing a file")
+        print(fnf_error)
+        raise FileNotFoundError
 
 
 def parse_template(string):
     """
-    This function will parse the template that was read in the read_template
-    function.
+    INPUT >> raw string from read_template function
+    OUTPUT >> tuple with two parts
+        - stripped string
+        - a tuple with the values taken out
     """
-    new_string = string
 
-    parts = tuple(re.findall(r"\{(.*?)\}", new_string))
-    string = re.sub(r"\{(.*?)\}", "{}", new_string)
+    pattern = r"\{([\w\s\d\'\-]+)\}"
+    words_list = re.findall(pattern, string)
+    words_tuple = tuple(words_list)
 
-    print(string, parts)
-    return string, parts
+    replace = r"\{([\w\s\d\'\-]+)\}"
+    new_string = re.sub(replace, "{}", string)
+
+    return (new_string, words_tuple)
 
 
-def merge(narrative, user_input):
+def merge(string, user_input):
     """
-     This function will combine user input with the given story and return it.
-     """
-    complete_narrative = narrative.format(*user_input)
-    return complete_narrative
+    INPUTS
+      - the template string
+      - a tuple with user input
+    OUTPUT >> a combined string
+    """
+
+    merged_string = string.format(*user_input)
+    return merged_string
 
 
-def main(path):
-    template = read_template(path)
+def ask_user_questions(words_tuple):
+    """
+    INPUT >> tuple - keywords from template
+    OUTPUT >> tuple of user answers
+    """
 
-    string, parts = parse_template(template)
+    answers = []
+    for words in words_tuple:
+        response = input(f"Please enter a {words} >> ")
+        answers.append(response)
 
-    new_parts = []
+    answer_tuple = tuple(answers)
+    return answer_tuple
 
-    for word in parts:
-        new_word = input(f"Enter a {word}. ")
-        new_parts.append(new_word)
 
-    madlib = merge(string, new_parts)
+def save_file(final):
+    with open("assets/users_custom_file.txt", "w") as new_file:
+        new_file.write(final)
 
-    with open('assets/new_file.txt', 'w') as file:
-        file.write(madlib)
 
-    print(f'Your madlib is: {madlib}')
+def initialize(template_path):
+    print_welcome_message()
+    raw_template = read_template(template_path)
+    stripped_string, key_words = parse_template(raw_template)
+    user_response = ask_user_questions(key_words)
+    merged_user_input = merge(stripped_string, user_response)
+
+    print(merged_user_input)
+    save_file(merged_user_input)
 
 
 if __name__ == "__main__":
-    greeting_msg()
-    path = 'assets/make_me_a_video_game_template.txt'
-    main(path)
+    initialize("assets/template_madlib.txt")
